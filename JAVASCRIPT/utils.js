45eae5ce55e14df7b5e00783226b14caeb1eb216
Judicael <judi.randriamanjato@gmail.com>
2023-12-07 14:38:59 +0300

    feat: wallet implementation in javascript

---
+++ JAVASCRIPT/utils.js
@@ -0,0 +1,14 @@
+function formatDate(date) {
+    return date.toLocaleString('en-US', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      timeZoneName: 'short'
+    });
+  }
+  
+  module.exports = { formatDate };
+  
