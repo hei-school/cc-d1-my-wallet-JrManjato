3affb4a093bf9f987df16d9f5eaf43627b7e5f6e
Judicael <judi.randriamanjato@gmail.com>
2023-12-07 14:33:56 +0300

    chore: delete all directories and move them to a specific branch

--- JAVA/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Wallet wallet = new Wallet();
-        Menu.startMenu(wallet);
-    }
-}
--- PYTHON/utils.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-def format_date(date):
-    return date.strftime('%d/%m/%Y %H:%M:%S')
